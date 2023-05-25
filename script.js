1c28b3ed3938098bf350a7b48dc0aa523859bcf0
LarryByrd89 <laryn.n99@gmail.com>
2023-05-25 00:12:18 -0400

    issue with city popln

--- script.js
+++ script.js
@@ -1,70 +1,85 @@
 let store;
-let history = document.querySelector('#history');
+let historyList = document.querySelector('#search-history-list');
 
 const getHistory = () => {
-    store = localStorage.history ? JSON.parse(localStorage.history) : [];
+  store = localStorage.history ? JSON.parse(localStorage.history) : [];
 
-    store.forEach(city => {
-        history.innerHTML += `<button onclick="searchHistory('${city}')">${city}</button>`;
-    });
+  store.forEach((city) => {
+    historyList.innerHTML += `<li class="list-group-item"><button onclick="searchHistory('${city}')">${city}</button></li>`;
+  });
 };
 
 getHistory();
 
-const searchHistory = city => {
-    document.querySelector('input').value = city;
-    searchCity();
+const searchHistory = (city) => {
+  document.querySelector('#search-city').value = city;
+  searchCity();
 };
 
-const searchCity = async () => {
-    let city = document.querySelector('input').value;
+const searchCity = () => {
+  let city = document.querySelector('#search-city').value;
 
-    if (!city) return;
+  if (!city) return;
 
-    let url1 = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=23d68278b5b786d3d93337f17d67f78a`;
+  let url2 = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=23d68278b5b786d3d93337f17d67f78a`;
 
-    let url2 = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=23d68278b5b786d3d93337f17d67f78a`;
+  if (!store.includes(city)) {
+    store.push(city);
+    localStorage.history = JSON.stringify(store);
+    historyList.innerHTML += `<li class="list-group-item"><button onclick="searchHistory('${city}')">${city}</button></li>`;
+  }
 
-    if (!store.includes(city)) {
-        store.push(city);
-        localStorage.history = JSON.stringify(store);
-        history.innerHTML += `<button onclick="searchHistory('${city}')">${city}</button>`;
-    };
+  fetch(url2)
+    .then((response) => response.json())
+    .then((data) => {
+      const { lat, lon } = data.city.coord;
+      let url1 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=23d68278b5b786d3d93337f17d67f78a`;
 
-    let { name, dt, main: { temp, humidity }, wind: { speed }, weather: [{ icon }] } = await (await fetch(url1)).json();
+      fetch(url1)
+        .then((response) => response.json())
+        .then((data) => {
+          let { name, dt, main: { temp, humidity }, wind: { speed }, weather: [{ icon }] } = data.list[0];
 
-    let current = document.querySelector('#current');
-    current.innerHTML = `
-        <h1>${name} (${new Date(dt * 1000).toLocaleDateString()}) <img src="https://openweathermap.org/img/w/${icon}.png" alt="${icon}"></h1>
-        <h3>Temperature: ${temp}°F</h3>
-        <h3>Humidity: ${humidity}%</h3>
-        <h3>Wind Speed: ${speed} MPH</h3>
-    `;
+          let currentCity = document.querySelector('#current-city');
+          currentCity.innerHTML = `City Information <small class="text-muted">${new Date(dt * 1000).toLocaleDateString()}</small>`;
 
-    let { list } = await (await fetch(url2)).json();
+          let currentTemp = document.querySelector('#current-temp');
+          currentTemp.textContent = `${temp}°F`;
 
-    let forecast = document.querySelector('#forecast');
-    forecast.innerHTML = '';
-    for (let i = 0; i < list.length; i = i + 8) {
-        let { dt, main: { temp, humidity }, weather: [{ icon }] } = list[i];
+          let currentHumidity = document.querySelector('#current-humidity');
+          currentHumidity.textContent = `${humidity}%`;
 
-        forecast.innerHTML += `
-            <div class="card">
-                <h3>${new Date(dt * 1000).toLocaleDateString()}</h3>
-                <img src="https://openweathermap.org/img/w/${icon}.png" alt="${icon}">
-                <h5>Temp: ${temp}°F</h5>
-                <h5>Humidity: ${humidity}%</h5>
-                <h5>Wind Speed: ${speed} MPH</h5>        
-            </div>
-        `;
-    }
-    console.log(list);
+          let currentWindSpeed = document.querySelector('#current-wind-speed');
+          currentWindSpeed.textContent = `${speed} MPH`;
+
+          let forecast = document.querySelector('#five-day-forecast');
+          forecast.innerHTML = '';
+
+          for (let i = 0; i < data.list.length; i += 8) {
+            let { dt, main: { temp, humidity }, weather: [{ icon }] } = data.list[i];
+
+            forecast.innerHTML += `
+              <div class="col-2">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">${new Date(dt * 1000).toLocaleDateString()}</h5>
+                    <img src="https://openweathermap.org/img/w/${icon}.png" alt="${icon}">
+                    <p class="card-text">Temp: ${temp}°F</p>
+                    <p class="card-text">Humidity: ${humidity}%</p>
+                  </div>
+                </div>
+              </div>
+            `;
+          }
+        })
+        .catch((error) => {
+          console.log('Error retrieving forecast data:', error);
+        });
+    })
+    .catch((error) => {
+      console.log('Error retrieving city data:', error);
+    });
 };
-
-
-
-
-
 
 
 
