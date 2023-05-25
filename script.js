e0303e3034a524c9bec65299bbb4dee2f02f74f3
LarryByrd89 <laryn.n99@gmail.com>
2023-05-25 00:35:32 -0400

    more java work

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 
+// Easy access to elements
 var searchHistoryList = $('#search-history-list');
 var searchCityInput = $("#search-city");
 var searchCityButton = $("#search-city-button");
 var clearHistoryButton = $("#clear-history");
-
 var currentCity = $("#current-city");
 var currentTemp = $("#current-temp");
 var currentHumidity = $("#current-humidity");
 var currentWindSpeed = $("#current-wind-speed");
 var UVindex = $("#uv-index");
-
 var weatherContent = $("#weather-content");
-
 var APIkey = "23d68278b5b786d3d93337f17d67f78a";
-
 var cityList = [];
 
 var currentDate = moment().format('L');
 $("#current-date").text("(" + currentDate + ")");
+
+initalizeHistory();
+showClear();
+
+$(document).on("submit", function(){
+    event.preventDefault();
+    var searchValue = searchCityInput.val().trim();
+    currentConditionsRequest(searchValue)
+    searchHistory(searchValue);
+    searchCityInput.val(""); 
+});
 
 
 
