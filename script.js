cafa673c4c4662861e18933e0f6aad044201ead3
LarryByrd89 <laryn.n99@gmail.com>
2023-05-25 23:15:15 -0400

    css edits

--- script.js
+++ script.js
@@ -1,32 +1,80 @@
+let store;
+let historyElement = document.querySelector('#history');
 
-// Easy access to elements
-var searchHistoryList = $('#search-history-list');
-var searchCityInput = $("#search-city");
-var searchCityButton = $("#search-city-button");
-var clearHistoryButton = $("#clear-history");
-var currentCity = $("#current-city");
-var currentTemp = $("#current-temp");
-var currentHumidity = $("#current-humidity");
-var currentWindSpeed = $("#current-wind-speed");
-var UVindex = $("#uv-index");
-var weatherContent = $("#weather-content");
-var APIkey = "23d68278b5b786d3d93337f17d67f78a";
-var cityList = [];
+const apiKey = '23d68278b5b786d3d93337f17d67f78a';
+const current = document.querySelector('#current');
+const forecast = document.querySelector('#forecast');
 
-var currentDate = moment().format('L');
-$("#current-date").text("(" + currentDate + ")");
+const getHistory = () => {
+    store = localStorage.history ? JSON.parse(localStorage.history) : [];
 
-initalizeHistory();
-showClear();
+    store.forEach(city => {
+        historyElement.innerHTML += `<button onclick="searchHistory('${city}')">${city}</button>`;
+    });
+};
 
-$(document).on("submit", function(){
-    event.preventDefault();
-    var searchValue = searchCityInput.val().trim();
-    currentConditionsRequest(searchValue)
-    searchHistory(searchValue);
-    searchCityInput.val(""); 
-});
+getHistory();
 
+const searchHistory = city => {
+    document.querySelector('input').value = city;
+    searchCity();
+};
+
+const searchCity = async () => {
+    let city = document.querySelector('input').value;
+
+    if (!city) return;
+
+    let url1 = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial&q=${city}`;
+    let url2 = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&units=imperial&q=${city}`;
+
+    if (!store.includes(city)) {
+        store.push(city);
+        localStorage.history = JSON.stringify(store);
+        historyElement.innerHTML += `<button onclick="searchHistory('${city}')">${city}</button>`;
+    }
+
+    let { name, dt, main: { temp, humidity }, wind: { speed }, weather: [{ icon }] } = await (await fetch(url1)).json();
+
+    current.innerHTML = `
+        <h1>${name} (${new Date(dt * 1000).toLocaleDateString()}) <img src="https://openweathermap.org/img/w/${icon}.png" alt="${icon}"></h1>
+        <h3>Temperature: ${temp}°F</h3>
+        <h3>Humidity: ${humidity}%</h3>
+        <h3>Wind Speed: ${speed} MPH</h3>
+        <h3>Current Time: ${getCurrentTime()}</h3>
+    `;
+
+    let { list } = await (await fetch(url2)).json();
+
+    forecast.innerHTML = '';
+    for (let i = 0; i < list.length; i = i + 8) {
+        let { dt, main: { temp, humidity }, weather: [{ icon }] } = list[i];
+
+        forecast.innerHTML += `
+            <div class="card">
+                <h3>${new Date(dt * 1000).toLocaleDateString()}</h3>
+                <img src="https://openweathermap.org/img/w/${icon}.png" alt="${icon}">
+                <h5>Temp: ${temp}°F</h5>
+                <h5>Humidity: ${humidity}%</h5>
+                <h5>Wind Speed: ${speed} MPH</h5>        
+            </div>
+        `;
+    }
+};
+
+function getCurrentTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var amPm = hours >= 12 ? 'PM' : 'AM';
+    
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    
+    var currentTime = hours + ':' + minutes + ' ' + amPm;
+    return currentTime;
+  }
 
 
 
